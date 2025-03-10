3e527e79b4b75b3a765fa03b663dff41c27e0d7f
Emma Ann Hynes <ekiizu2003@gmail.com>
2025-03-10 21:50:39 +0000

    added components for profile sections, need to fix position of notifications

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -11,87 +11,65 @@ const ProfileComponent = () => {
     "images/ozzybaby.jpg",
     "images/ozbox.jpg",
   ];
-    
-
 
   return (
+    <div className="max-w-2xl mx-auto rounded-lg overflow-hidden">
+      {/* Banner */}
+      <div className="relative">
+        <img
+          src="images/ozzypaw.jpg"
+          alt="Banner"
+          className="h-48 w-full object-cover"
+        />
+        <img
+          src="images/ozzy.jpg"
+          alt="User Profile"
+          className="w-24 h-24 rounded-full object-cover border-4 border-white absolute left-1/2 transform -translate-x-1/2 -bottom-12 shadow-md ml-50"
+        />
+      </div>
 
-    <div className="banner">
-       <img
-         src="images/ozzypaw.jpg"
-         alt="Banner"
-         className="h-48 w-full object-cover rounded-t"
-       />
-       
-    
-    
-    <div className="p-4 flex flex-col items-center">
-    <img
-      src="images/ozzy.jpg"
-      alt="User Profile"
-      className="w-24 h-24 rounded-full object-cover border-4 border-white -mt-12 ml-100"
-    />
-    
+      <div className="p-6 text-center">
+        {/* User Info */}
+        <h1 className="text-2xl font-bold">Ekiizu </h1>
+        <h3 className="text">@Ekiizu</h3>
 
-    <h1 className="text-xl font-bold mt-2 text-primary">
-      Ekiizu
-    </h1>
-    
-    <h3 className="text-primary"> @Ekiizu </h3>
-    <div
-        tabIndex={0}
-        className="bg-primary text-primary-content focus:bg-secondary focus:text-secondary-content collapse"
-        >
-        <div className="collapse-title font-semibold">
-            <button className="px-4 py-2 bg-primary text-white rounded-lg">Edit</button>
-        </div>
-        
-        <div className="collapse-content text-sm">
-
-
-        
-        <button
-        className="px-4 py-2 bg-primary text-white rounded-lg"
-        onClick={() => navigate("/Login")}
-      >
-        Logout
-      </button>
-        </div>
+        {/* Edit and Logout Buttons */}
+        <div className="mt-4 flex justify-center space-x-3">
+          <button className="px-4 py-2 bg-primary text-white rounded-lg shadow-md hover:bg-yellow-600 transition">
+            Edit
+          </button>
+          <button className="px-4 py-2 bg-primary text-white rounded-lg shadow-md hover:bg-yellow-600 transition">
+            Logout
+          </button>
         </div>
 
-    {/* Social Links */}
-    <div className="flex justify-center space-x-4 mt-3 text-primary">
-      <a href="#" className="hover:underline">Instagram</a>
-      <a href="#" className="hover:underline">Twitter</a>
-      <a href="#" className="hover:underline">Ko-fi</a>
-    </div>
-    <div className="w-130 border-2 border-black rounded">
-    <p className="mt-2 text-sm text-gray-500">HELLLOOOO!!!!!!!!!!!</p>
-    
-    {/* Spotify Embed */}
-    <div className="mt-4 w-100">
-      <iframe
-        src="https://open.spotify.com/embed/track/32NyN0Tby1YpKObfd8nrzN?utm_source=generator"
-        width="80%"
-        height="80"
-        frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-      ></iframe>
-    </div>
+        {/* Social Links */}
+        <div className="flex justify-center space-x-6 mt-4 text-primary">
+          <a href="#" className="hover:underline font-medium">Instagram</a>
+          <a href="#" className="hover:underline font-medium">Twitter</a>
+          <a href="#" className="hover:underline font-medium">Ko-fi</a>
+        </div>
 
-    {/* Buttons */}
-    <div className="mt-4">
-      
-      
-      
+        {/* Bio Section */}
+        <div className="mt-6 p-4 border-t">
+          <p className="text text-sm">HELLLOOOO!!!!!!!!!!!</p>
+        </div>
+
+        {/* Spotify Embed */}
+        {/* <div className="mt-4">
+          <iframe
+            src="https://open.spotify.com/embed/track/32NyN0Tby1YpKObfd8nrzN?utm_source=generator"
+            width="100%"
+            height="80"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            className="rounded-lg shadow-md"
+          ></iframe>
+        </div> */}
+      </div>
     </div>
-  </div>
-</div>
-</div>
-
-
-    
   );
-}
+};
 
 export default ProfileComponent;
+
