8af293a143d9e341245dbafba765d611a2cb21f3
RyanCri <N00221108@iadt.ie>
2025-03-21 22:37:40 +0000

    remove card from posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,11 +7,11 @@ const Post = (postInfo) => {
     const info = postInfo.postInfo;
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
+        <div className="">
         <figure>
             <img
             src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-            alt="Shoes" />
+            alt="Shoes" /> 
         </figure>
         <div className="card-body">
             <h2 className="card-title">{info.user_id}</h2>
