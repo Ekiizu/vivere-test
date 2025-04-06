a93d2e7786db89786f358de731ceedbf6b0e16aa
RyanCri <N00221108@iadt.ie>
2025-04-06 12:53:01 +0100

    add like button component with live update on like count, bug with delete sometimes not refreshing

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,7 @@
 import { React, useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import LikeButton from "./LikeButton";
 import "../App.css";
 
 // This is just the text and info for the posts. Images will be added in the home page. To avoid unnecessary API calls
@@ -86,7 +87,7 @@ const Post = (postInfo) => {
           })
           .catch(err => console.log(err))
         
-          likeCount += 1
+          // setLikeCount(likeCount += 1)
       }
       else if (action == "unlike") {
         // we want to destroy ALL likes from this user if for whatever reason more they have liked more than once
@@ -103,7 +104,7 @@ const Post = (postInfo) => {
             .catch(err => console.log(err))
         });
 
-        likeCount -= 1
+        // setLikeCount(likeCount -= 1)
       }
     }
 
@@ -147,34 +148,7 @@ const Post = (postInfo) => {
             
 
           <div className="flex justify-end p-4">
-          <label id="likeButton" className=" mr-8 swap"  >
-            {/* checked="true" to make like button start ON */}
-            {hasLiked.length != 0 
-            ? <input id="likeButton" type="checkbox" defaultChecked/>
-            : <input id="likeButton" type="checkbox" />
-            }
-            
-            
-            <div id="likeButton" className="swap-off">
-            <svg id="likeButton" onClick={() => handleLike("unlike")}xmlns="http://www.w3.org/2000/svg" className="stroke-primary" viewBox="0 0 24 24" width="48" height="48">
-              <path id="likeButton" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="none" stroke-width="2"/>
-            </svg>
-            {/* conditional rendering on if likes is defined for like count showing up */}
-            {likes != null &&
-            <p className="text-primary">{likes.length} likies</p>
-            }
-
-            </div>
-            <div id="likeButton" onClick={() => handleLike("like")} className="swap-on">
-              <svg id="likeButton" xmlns="http://www.w3.org/2000/svg" className="fill-primary stroke-primary" viewBox="0 0 24 24" width="48" height="48">
-              <path id="likeButton" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" stroke-width="2"/>
-            </svg>
-            {likes != null &&
-            <p className="text-primary">{likes.length} likies</p>
-            }
-
-            </div>
-          </label>
+          <LikeButton info={info} />
           </div>
         </div>
     )
