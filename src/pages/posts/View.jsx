ceee54f377916dd2ee88f07f994f3507de33735c
RyanCri <N00221108@iadt.ie>
2025-04-19 13:24:38 +0100

    add comment posting from front-end

--- src/pages/posts/View.jsx
+++ src/pages/posts/View.jsx
@@ -17,7 +17,7 @@ function ViewPost() {
 
   const [post, setPost] = useState(null)
   const [postImages, setPostImages] = useState(null)
-  const [comments, setComments] = useState()
+  const [comment, setComment] = useState()
   const [replies, setReplies] = useState ([])
 
   const navigate = useNavigate();
@@ -105,6 +105,30 @@ function ViewPost() {
     })
   }
 
+  const handleComment = async (e) => {
+    setComment(e.target.value)
+  }
+
+  const submitComment = async () => {
+    const form = {
+      post_id: id,
+      user_id: user.id,
+      body: comment
+    }
+    
+    axios.post(`https://viverebackend-main-girysq.laravel.cloud/api/comments`, form, {
+      headers: {
+          Authorization: `Bearer ${token}`
+      }
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+  }
+
   if(post != null ) {
     return post && (
       // this is to confine the post area within the right space
@@ -144,12 +168,13 @@ function ViewPost() {
               </div>
               <Post postInfo={post}/>
               
+              {/* comment input */}
               <div className="join px-8 pt-4 w-full">
               <label className="input join-item w-full pr-0">
               <svg className="fill-primary" width="24px" height="24px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>comment 3</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-207.000000, -257.000000)"> <path d="M231,273 C229.896,273 229,272.104 229,271 C229,269.896 229.896,269 231,269 C232.104,269 233,269.896 233,271 C233,272.104 232.104,273 231,273 L231,273 Z M223,273 C221.896,273 221,272.104 221,271 C221,269.896 221.896,269 223,269 C224.104,269 225,269.896 225,271 C225,272.104 224.104,273 223,273 L223,273 Z M215,273 C213.896,273 213,272.104 213,271 C213,269.896 213.896,269 215,269 C216.104,269 217,269.896 217,271 C217,272.104 216.104,273 215,273 L215,273 Z M223,257 C214.164,257 207,263.269 207,271 C207,275.419 209.345,279.354 213,281.919 L213,289 L220.009,284.747 C220.979,284.907 221.977,285 223,285 C231.836,285 239,278.732 239,271 C239,263.269 231.836,257 223,257 L223,257 Z" id="comment-3" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>
-                <input type="text" placeholder="Add a comment..." className="" autoComplete="off"/>
+                <input type="text" placeholder="Add a comment..." className="" autoComplete="off" onChange={handleComment}/>
                 </label>
-                <button className="btn btn-neutral join-item">Post</button>
+                <button className="btn btn-neutral join-item" onClick={submitComment}>Post</button>
 
               </div>
 
@@ -157,7 +182,6 @@ function ViewPost() {
             </div>
 
             {post.comments != null && <h1 className="text-xl font-bold mx-8 text-primary">{post.comments.length} comments</h1>}
-
 
             {post.comments != null && post != null &&
             post.comments.map(({id, body, user_id, post_id}, j) => {
